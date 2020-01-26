2b202e156e9d04f7697814241b665846d16ac831
Thom Mondeaux <Thommond554@gmail.com>
2020-01-25 21:27:29 -0500

    I added several webpages including resume.html which is currently empty. I also created bio.html which is my proffesional blog site that includes one entry so far. I made a script for my javaScript functinality I plan to add soon. In addition I added a plethera of educational accoplishments to the mywork.html page. Lastly,  I updated some of my navigation and added meta viewports to the pages.

---
+++ script.js
@@ -0,0 +1,14 @@
+
+// This function allows the sandwich menu to open
+let navOpen = () => {
+  const smallNav = document.querySelector('menu')
+  smallNav.style.width = '100%';
+
+};
+// This function closes the sandwich nav menu
+
+let navClose = () => {
+  const smallNav = document.querySelector('menu')
+  smallNav.style.width = '0%';
+
+};
