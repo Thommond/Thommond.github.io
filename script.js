4f45970cd27d78ce7739098c34bf05c246599688
Thom Mondeaux <Thommond554@gmail.com>
2020-02-09 17:04:33 -0500

    I added more to my resume and work files. I added skills, soft skills and updated some styles relating to those pages.
    In addition I made my navigation bar more responsive. Also I added my
    proffesional picture instead of the previous logo. I deleted some uneeded
    styles and scripts.

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-
-// This function allows the sandwich menu to open
-let navOpen = () => {
-  const smallNav = document.querySelector('menu')
-  smallNav.style.width = '100%';
-
-};
-// This function closes the sandwich nav menu
-
-let navClose = () => {
-  const smallNav = document.querySelector('menu')
-  smallNav.style.width = '0%';
-
-};
+/*if needed*/
